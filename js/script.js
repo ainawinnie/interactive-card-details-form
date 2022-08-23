13db05d822b22256bc35c349c2cd134aef51f9f1
Aina <aina.winnie@gmail.com>
2022-08-22 21:57:52 -0300

    Starts JS

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/*Card Data */
+const partOne = document.getElementById('part-one');
+const partTwo = document.getElementById('part-two');
+const partThree = document.getElementById('part-three');
+const partFour = document.getElementById('part-four');
+const nameCard = document.getElementById('name-card');
+const monthCard = document.getElementById('month-card');
+const yearCard = document.getElementById('year-card');
+const cvcCard = document.getElementById('cvc-card');
+
+/*Form data */
+const nameInput = document.getElementById('name-input');
+const cardNumber = document.getElementById('card-number-input');
+const monthInput = document.getElementById('month');
+const yearInput = document.getElementById('year');
+const cvcInput = document.getElementById('cvc-input');
+const sendButton = document.getElementById('send-button');
+
+console.log(nameCard.value)
+/* Receives user name*/
+if (nameInput.va) {
+    console.log('Aqui')
+}
