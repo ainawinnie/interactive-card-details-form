147e3a59af0767625a03831584e56de15500f794
Aina <aina.winnie@gmail.com>
2022-08-24 21:40:06 -0300

    Continues field validations

--- js/script.js
+++ js/script.js
@@ -16,18 +16,15 @@ const yearInput = document.getElementById('year');
 const cvcInput = document.getElementById('cvc-input');
 const sendButton = document.getElementById('send-button');
 
-const teste = document.getElementById('testeabc');
 /* Receives user name*/
-
-console.log(nameInput.parentNode.childNodes)
 
 function initApp() {
     addEventInElements(nameInput);
-    addEventInElements(cardNumber);
-    addEventInElements(monthInput);
-    addEventInElements(yearInput );
     addEventInElements(cvcInput);
     addEventInElements(sendButton);
+    addEventInDateElements(monthInput);
+    addEventInDateElements(yearInput );
+    addEventInNumberElements(cardNumber);
 
     
 };
@@ -63,6 +60,59 @@ function removeErrors (input, elements) {
     input.parentNode.children[2].classList.add('hide');
 }
 
+
+function addEventInDateElements (element) {
+    element.addEventListener('blur', function() {
+        if(element) {
+            validateEmptyDateField(element);
+
+        } else {
+            isEmailValid(element)
+        }
+    })
+
+}
+
+function validateEmptyDateField (input) {
+    if (!input.value || input.value.length < 1) {
+        addDateErrors(input);
+    } else {
+        removeDateErrors(input);
+    }
+}
+
+function addDateErrors (input) {
+    input.classList.add('input-error');
+    input.parentNode.parentNode.children[1].classList.remove('hide');
+}
+
+function removeDateErrors (input, elements) {
+    input.classList.remove('input-error');
+    input.parentNode.children[2].classList.add('hide');
+}
+
+function addEventInNumberElements(element) {
+    element.addEventListener('blur', function() {
+        if(element) {
+            validateNumberField(element);
+
+        } else {
+            isEmailValid(element)
+        }
+    })
+
+}
+function validateNumberField (input) {
+    if (!input.value || input.value.length < 1) {
+        addErrors(input);
+    } else {
+        removeErrors(input);
+        if (isNaN(input)) {
+            console.log(isNaN(input))
+            input.parentNode.children[3].classList.remove('hide');            
+        }
+    }
+}
 
 
 initApp()
